a8fe8500fa54e6bf28a8f1d73eaeba39191c2a70
Hunter Renard <renardhj@gmail.com>
2018-10-06 18:29:25 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ let size;
 let input;
 
 function setup() {
+  // sets up canvas
   createCanvas(800, 800);
   background(255, 255, 255);
   rectMode(CENTER);
@@ -12,19 +13,21 @@ function setup() {
   colorMode(HSB, 100, 100, 100)
   noFill();
 
+  // creates input for user
   input = createInput();
   button = createButton('submit');
-  button.mousePressed(greet);
+  button.mousePressed(generateColors);
 
   validChars = 'abcdefghijklmnopqrstuvwxyz';
+
+  // read in text file and display
   readTextFile("text.txt");
   txt = "";
-
   while(typeof origTxt == 'undefined')
   {
     console.log("test");
   }
-
+  
   for (let i = 0; i < origTxt.length; i++) {
     if(validChars.includes(origTxt[i].toLowerCase()))
       txt += origTxt[i].toLowerCase();
@@ -39,20 +42,22 @@ function setup() {
   }
 }
 
-function greet() {
-
+function generateColors() {
   origTxt = '';
   txt = '';
   origTxt = input.value();
-  console.log(origTxt);
 
   background(255, 0, 255);
+  
+  // set and clean text values
   for (let i = 0; i < origTxt.length; i++) {
     if(validChars.includes(origTxt[i].toLowerCase()))
       txt += origTxt[i].toLowerCase();
   }
 
   let size = txt.length;
+  
+  // generate colors from character ascii values
   for (let i = 0; i < txt.length; i++) {
     let c = map(txt[i].charCodeAt(0), 97, 122, 0, 100);
     stroke(c, 100, 100);
@@ -61,6 +66,7 @@ function greet() {
   }
 }
 
+// reads in a text file
 function readTextFile(file)
 {
     var rawFile = new XMLHttpRequest();
