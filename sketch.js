a6761d5eeb45d1eb29d31153ab97f15a4ec2df4f
Hunter Renard <renardhj@gmail.com>
2018-10-08 09:02:41 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,29 +5,25 @@ let size;
 let input;
 
 function setup() {
-  // sets up canvas
   createCanvas(800, 800);
   background(255, 255, 255);
   rectMode(CENTER);
   translate(width / 2, height / 2);
   colorMode(HSB, 100, 100, 100)
-  noFill();
 
-  // creates input for user
   input = createInput();
   button = createButton('submit');
   button.mousePressed(generateColors);
 
   validChars = 'abcdefghijklmnopqrstuvwxyz';
-
-  // read in text file and display
   readTextFile("text.txt");
   txt = "";
+
   while(typeof origTxt == 'undefined')
   {
-    console.log("test");
+
   }
-  
+
   for (let i = 0; i < origTxt.length; i++) {
     if(validChars.includes(origTxt[i].toLowerCase()))
       txt += origTxt[i].toLowerCase();
@@ -37,36 +33,37 @@ function setup() {
   for (let i = 0; i < txt.length; i++) {
     let c = map(txt[i].charCodeAt(0), 97, 122, 0, 100);
     stroke(c, 100, 100);
-    rect(0, 0, size, size);
+    fill(c, 100, 100);
+    let s = map(size, 0, txt.length, 0, 800);
+    rect(0, 0, s, s);
     size--;
   }
 }
 
 function generateColors() {
+
   origTxt = '';
   txt = '';
   origTxt = input.value();
+  console.log(origTxt);
 
   background(255, 0, 255);
-  
-  // set and clean text values
   for (let i = 0; i < origTxt.length; i++) {
     if(validChars.includes(origTxt[i].toLowerCase()))
       txt += origTxt[i].toLowerCase();
   }
 
   let size = txt.length;
-  
-  // generate colors from character ascii values
   for (let i = 0; i < txt.length; i++) {
     let c = map(txt[i].charCodeAt(0), 97, 122, 0, 100);
     stroke(c, 100, 100);
-    rect(0, 0, size, size);
+    fill(c, 100, 100);
+    let s = map(size, 0, txt.length, 0, 800);
+    rect(0, 0, s, s);
     size--;
   }
 }
 
-// reads in a text file
 function readTextFile(file)
 {
     var rawFile = new XMLHttpRequest();
